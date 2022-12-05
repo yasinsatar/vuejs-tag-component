37912cffd91966c5e811cf0a2810f0aef1617b9e
Yasin Satar <yasinsatar@hotmail.com>
2022-12-05 11:32:14 +0300

    tag style and create

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,54 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="tag-container">
+  <span class="tag" v-for="tag in tags">
+    <span class="content">{{tag}}</span>
+    <span class="close">X</span>
+  </span>
+  <input type="text" @keydown.enter="addTag">
+</div>
 </template>
-
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+export default{
+  data(){
+    return{
+      tags:[]
+    }
+  },
+  methods:{
+    addTag(event){  
+      this.tags.push(event.target.value);
+      event.target.value="";
     }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: sans-serif;
 }
 
-h1, h2 {
-  font-weight: normal;
+.tag-container{
+  border:1px solid #ccc;
+  padding:20px;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.tag{
+  background-color: #fbbd08;
+  padding:10px;
+  cursor:default;
+  font-size: 14px;
+  margin-right:10px;
+  margin-bottom:10px;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.tag .close{
+  font-size:12px;
+  cursor:pointer;
 }
 
-a {
-  color: #42b983;
-}
+input{
+  outline:none;
+  height:30px;
+
+} 
 </style>
